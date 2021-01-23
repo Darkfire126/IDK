d5596addaee150694be709b6c1e9c6ca8e02e815
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-01-23 08:55:00 -0800

    npm

--- commands/Info/npm.js
+++ commands/Info/npm.js
@@ -2,8 +2,9 @@ module.exports = {
     category: "Info",
     description: "NPM packages",
     callback: async ({ message, args, text, client, prefix, instance }) => {
-    const package = args[0]
+    const package = args.slice(0).join
     if(!package) return User.send("Please use a npm package example: <PREFIX>npm discord.js")
+    message.channel.send(`https://npmjs.com/package/${package}`)
     
     
     
