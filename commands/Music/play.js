d73e7837fb0485779fc5e9e51b0b4e3fec08e2eb
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-01-23 08:17:17 -0800

    tt

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -2,5 +2,5 @@ module.exports = {
     category: "Music",
     description: "plays some music",
     callback: async ({ message, args, text, client, prefix, instance }) => {
-distube.play(message, args.join(" "));
+client.distube.play(message, args.join(" "));
     }}
