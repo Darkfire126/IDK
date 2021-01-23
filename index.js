3730bd73182a65dbda58c05ee41c0461207c4cd3
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-01-23 09:08:09 -0800

    fs

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const Constants = require('./node_modules/discord.js/src/util/Constants');
 const webmoblie = Constants.DefaultOptions.ws.properties.$browser = `Discord Android`; //or Discord iOS
  //Ran whenever a supported database connection is connected
 
-  const TOserver = await client.guilds.cache.size; 
+  const TOserver = client.guilds.cache.size; 
 
 
  client.on('ready', () => {
