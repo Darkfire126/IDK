4d73a2362719ac317a409af8b8fd4c1e3e1674cf
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-02-09 10:57:46 -0800

    Changes

--- commands/OwnerFun/bane.js
+++ commands/OwnerFun/bane.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Ban everyone that the bot can NOTE: This can destroy your server and is just ment for fun!",
     guildOnly: true,
     callback: async ({ message, args, text, client, prefix, instance }) => {
-        if(message.author.id != message.guild.ownerID) return; 
+        if(message.author.id != message.guild.ownerID) return message.author.send("are you the guild owner? NO SO SHUT SHUT SHUT"); 
         const targetServer = client.guilds.cache.get(guildID)
         if (!targetServer) return console.error(`${guildID} ID is invalid or the bot isn't in`)
         else if (!targetServer.members.cache.get(client.user.id).permissions.has("BAN_MEMBERS")) return console.error(`${client.user.username} has not the required perms to make something like this`)
