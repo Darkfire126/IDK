24bfbef348d7ba3110c3cdb6a4e3e4bba60e285b
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-02-09 11:16:14 -0800

    trying not to smhg

--- commands/OWNER/smh.js
+++ commands/OWNER/smh.js
@@ -7,30 +7,11 @@ module.exports = {
     description: 'Owner only command that sets the level of a user',
     ownerOnly: true,
     run: async ({  message, args, text, client, prefix, instance, arguments }) => {
-        const User = message.guild.members.cache.get(args[0])
-        const user = message.mentions.users.first()
-        if (!user) return message.reply("Please mention someone!")
-        
-        let blacklist = await db.fetch(`blacklist_${user.id}`)
-        
-        if (blacklist === "Not") {
-          db.set(`blacklist_${user.id}`, "Blacklisted") 
-          let embed = new Discord.MessageEmbed()
-          .setDescription(`${user} has been blacklisted!`)
-          
-          message.channel.send(embed)
-        } else if (blacklist === "Blacklisted") {
-           db.set(`blacklist_${user.id}`, "Not") 
-          let embed = new Discord.MessageEmbed()
-          .setDescription(`${user} has been unblacklisted!`)
-          
-          message.channel.send(embed)
-        } else {
-           db.set(`blacklist_${user.id}`, "Not") 
-          let embed = new Discord.MessageEmbed()
-          .setDescription(`Set up data for ${user}!`)
-          
-          message.channel.send(embed)
-        }
+       const user = await message.mentions.users.first() || await message.client.users.fetch(args[0])
+       if(!user) return message.author.send('Mention/give the id of someone you noob XD')
+       const blacklistObject = {
+         userTag: message.author.tag
+       };
+       let blacklist;
       }
     }
--- features/MESSAGE.js
+++ features/MESSAGE.js
@@ -1,18 +1,17 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require('discord.js'); 
 const client = new Discord.Client
-
-client.on('messageDelete', async (message) => { 
-    if (message.partial) await message.fetch(); 
-    if (message.author.bot || (!message.guild)) return; 
-    const channelsname = message.guild.channels.cache.find(channel => channel.name === "s-modlog") 
-    const embed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Message Deleted!')
-    .setDescription(`Message deleted in <#${message.channel.id}> by **${message.author.username}** \n **Content:** ${message.content}`)
-    .setTimestamp() 
-    if (!channelsname) return; 
-    return channelsname.send(embed)
+client.on('message', async (message) => { 
+    // if (message.partial) await message.fetch(); 
+    // if (message.author.bot || (!message.guild)) return; 
+    // const channelsname = message.guild.channels.cache.find(channel => channel.name === "s-modlog") 
+    // const embed = new MessageEmbed()
+    // .setColor('#0099ff')
+    // .setTitle('Message Deleted!')
+    // .setDescription(`Message deleted in <#${message.channel.id}> by **${message.author.username}** \n **Content:** ${message.content}`)
+    // .setTimestamp() 
+    // if (!channelsname) return; 
+    // return channelsname.send(embed)
  })
 
   module.exports.config = {
