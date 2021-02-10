19d0b927d5c0d54628785af3cc6356d2c5abfcb5
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-02-09 19:31:47 -0800

    wa

--- commands/Configuration/simjoin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    description: 'Sim a join',
-    category: 'Configuration',
-    requiredPermissions: ['ADMINISTRATOR'],
-    callback: (message, args, text, client) => {
-      client.emit('guildMemberAdd', message.member)
-    },
-  }
