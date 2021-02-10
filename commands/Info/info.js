31a4a67c4ff7a8fb6e117d265f57cf80febd7f49
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-02-10 14:09:21 -0800

    b

--- commands/Info/info.js
+++ commands/Info/info.js
@@ -10,7 +10,13 @@ module.exports = {
         .addField('Bot Tag: ', client.user.tag, true)
         .addField('Server Count: ', (await client.shard.fetchClientValues('guilds.cache.size')).reduce((a, b) => b + a), true)
         .addField('User Count: ', (await client.shard.fetchClientValues('users.cache.size')).reduce((a, b) => b + a), true)
-      .setThumbnail(client.user.displayAvatarURL())
+        .addField('Bot Developer Name: ', 'HB - Darkfire - PainXD')
+        .addField('Bot Developer Tag: ', 'HB - Darkfire - PainXD#1216')
+        .addField('Bot Developer ID: ', '725010310922240007')
+
+      
+      
+        .setThumbnail(client.user.displayAvatarURL())
         )
 
 
