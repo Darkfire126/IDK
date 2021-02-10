399caa3c41b192d0fc8af09250710254e4b3d676
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-02-10 13:41:34 -0800

    as

--- commands/Info/invite.js
+++ commands/Info/invite.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: ['inv'],
     callback: async ({ message, args, text, client, prefix, instance }) => {
         message.channel.send(`
-        Bot Invite: https://discord.com/oauth2/authorize?client_id=799779495581843575&scope=bot&permissions=805314622
-        Support Server Invite:  https://discord.gg/MVfMS96mRN
+        [**Bot Invite: **]( https://discord.com/oauth2/authorize?client_id=799779495581843575&scope=bot&permissions=805314622)
+        [**Support Server Invite: **](https://discord.gg/MVfMS96mRN)
         `)
     }}
---
+++ shard.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const {ShardingManager} = require('discord.js')
+const shards = new ShardingManager("./index.js", {
+    token: process.env.TOKEN, // If your token was inside .env, use process.env
+    totalShards: "auto"
+});
+ 
+shards.on("shardCreate", shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
+});
+ 
+shards.spawn(shards.totalShards, 10000); // For safety reason, try to use 10 seconds to respawn the shard.
